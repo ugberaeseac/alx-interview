74487d53912b09a106d7e41379ef3192d36434e6
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-15 17:46:21 -0500

    fixed order of display

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,19 +6,24 @@ const argv = myProcess.argv;
 
 if (argv.length === 3) {
   const url = 'https://swapi-api.alx-tools.com/api/films/' + argv[2];
-  myRequest(url, function (error, response, body) {
+
+  myRequest(url, async function (error, response, body) {
     if (error) {
       console.log(error);
     } else {
       const film = JSON.parse(body);
-      for (const i in film.characters) {
-        myRequest(film.characters[i], function (error, response, body) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(JSON.parse(body).name);
-          }
+
+      for (const character of film.characters) {
+        const promise = new Promise((resolve, reject) => {
+          myRequest(character, function (error, response, body) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(JSON.parse(body).name);
+            }
+          });
         });
+        console.log(await promise);
       }
     }
   });
