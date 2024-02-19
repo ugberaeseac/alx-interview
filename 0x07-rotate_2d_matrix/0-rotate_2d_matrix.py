155717a694e55a90ffb6d1bc6271590a710cded2
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-19 15:29:09 -0500

    Transpose a 2-D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Rotate a 2-D matrix 90 degrees clockwise(Transpose)
+Matrix must be edited in-place
+Matrix can be assumed to not be empty
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    rotate the 2-dimensional matrix
+    """
+    if matrix == []:
+        return
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
