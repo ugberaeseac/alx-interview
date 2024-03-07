7885aad050be8919bc333f4fbc114620cda9d9a7
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-03-07 17:57:30 -0500

    A function that returns the perimeter of the island described in grid

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+function that returns the perimeter of an island described in grid
+grid is a list of list of integers:
+
+    0 represents water
+    1 represents land
+    Each cell is square, with a side length of 1
+    Cells are connected horizontally/vertically (not diagonally).
+    grid is rectangular, with its width and height not exceeding 100
+returns thee perimeter of the island
+"""
+
+
+def island_perimeter(grid):
+    """
+    return the perimeter of the island
+    """
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
